9a2485e63bb590d41f62d3138f8c5508e3bf94e6
ac19-1 <audrey.chen@binus.ac.id>
2020-12-24 02:54:00 +0700

    initial commit

---
+++ assets/js/classification.js
@@ -0,0 +1,297 @@
+$(document).ready(() => {
+    step1();
+})
+
+function step1() {
+    $('#step-1-form').submit((e) => {
+        e.preventDefault()
+
+        $("#loading-text").text("Reading Data")
+        $("#loading").show()
+
+        var formData = new FormData(e.target)
+        $.ajax({
+            url: './controller/controller.php',
+            data: formData,
+            success: (response) => {
+                $("#loading").hide()
+
+                final_data = response
+
+                // reset table
+                resetTable()
+
+                // show train data
+                showResponse("#train-table", response, "train")
+
+                // show test data
+                showResponse("#test-table", response, "test")
+
+                $('#step-2').show(0, step2)
+                $('#loaded-data').show(0)
+                $('#step-1').hide(0)
+                $(window).trigger('resize');
+            },
+            error: () => {
+                $("#loading").hide()
+            },
+            dataType: 'json',
+            processData: false,
+            contentType: false,
+            method: 'POST'
+        })
+    })
+}
+
+function step2() {
+    const map_value = $("#mapping-value")
+    const header = Object.keys(final_data["train"][0])
+    map_value.find(".form-check").remove()
+    header.forEach((v) => {
+        map_value.append(`
+        <li class="form-check">
+            <input class="form-check-input" checked type="checkbox" value="${v}" id="${v}">
+            <label class="form-check-label" for="${v}">${v}</label>
+        </li>`)
+    })
+
+    $('#save-attribute-button').click((e) => {
+        $("#loading-text").text("Saving Attributes")
+        $("#loading").show()
+
+        let attributes = []
+        let children = map_value.children()
+        for (let i = 0; i < children.length; i++) {
+            let val = children.get(i).firstElementChild.value
+            let checked = children.get(i).firstElementChild.checked
+            attributes.push({
+                "val": val,
+                "checked": checked
+            })
+        }
+
+        $.ajax({
+            url: './controller/controller.php',
+            data: {
+                "request_data": final_data,
+                "attr": attributes,
+                "step": "2"
+            },
+            success: (response) => {
+                $("#loading").hide()
+                final_data = response;
+                // reset table
+                resetTable()
+
+                // show train data
+                showResponse("#train-table", final_data, "train")
+
+                // show test data
+                showResponse("#test-table", final_data, "test")
+
+                $('#step-3').show(0, step3)
+                $('#step-2').hide(0)
+            },
+            error: () => {
+                $("#loading").hide()
+            },
+            method: "POST",
+            dataType: "json"
+        })
+    })
+}
+
+function step3() {
+    const sel_label = $("#select-label")
+    const header = Object.keys(final_data["train"][0])
+    sel_label.find(".sel-label-div").remove()
+    header.forEach((v) => {
+        sel_label.append(`
+        <li class="form-check">
+            <input class="form-check-input" type="radio" name="label-radio" id="${v}-label" value="${v}">
+            <label class="form-check-label" for="${v}-label">${v}</label>
+        </li>`)
+    })
+
+    $('#save-label-button').click((e) => {
+        $("#loading-text").text("Setting Label")
+        $("#loading").show()
+
+        let children = sel_label.children()
+        for (let i = 0; i < children.length; i++) {
+            let val = children.get(i).firstElementChild.value
+            let checked = children.get(i).firstElementChild.checked
+            if (checked == true) {
+                label = val;
+            }
+        }
+
+        $.ajax({
+            url: './controller/controller.php',
+            data: {
+                "request_data": final_data,
+                "label": label,
+                "step": "3"
+            },
+            success: (response) => {
+                final_data_exclude_label = response;
+                step4();
+            },
+            error: () => {
+                $("#loading").hide()
+            },
+            method: "POST",
+            dataType: "json"
+        })
+    })
+}
+
+function step4() {
+    $("#loading-text").text("Parsing Nominal Data to Numeric")
+
+    $.ajax({
+        url: './controller/controller.php',
+        data: {
+            "request_data": final_data_exclude_label,
+            "step": "4"
+        },
+        success: (response) => {
+            parsed_data = response
+            step5()
+        },
+        error: () => {
+            $("#loading").hide()
+        },
+        method: "POST",
+        dataType: "json"
+    })
+}
+
+function step5() {
+    $("#loading-text").text("Calculating Distances")
+
+    $.ajax({
+        url: './controller/controller.php',
+        data: {
+            "request_data": parsed_data,
+            "final_data": final_data,
+            "label": label,
+            "step": "5"
+        },
+        success: (response) => {
+            $("#loading").hide()
+            final_data = response
+            // console.log(response)
+
+            // reset table
+            resetTable()
+
+            // show train data
+            showResponse("#train-table", final_data, "train")
+
+            // show test data
+            showResponse("#test-table", final_data, "test")
+
+            $('#result-chart').show(0, showChart)
+        },
+        error: () => {
+            $("#loading").hide()
+        },
+        method: "POST",
+        dataType: "json"
+    })
+}
+
+function showChart() {
+
+    $.ajax({
+        url: './controller/controller.php',
+        data: {
+            "final_data": final_data,
+            "step": "chart"
+        },
+        success: (response) => {
+            $('#step-3').hide(0)
+            new Chart(document.getElementById("bar-chart"), {
+                type: 'bar',
+                data: {
+                    labels: ["Chronic Kidney Disease", "Not Chronic Kidney Disease"],
+                    datasets: [
+                        {
+                            label: "Predicted People",
+                            backgroundColor: ["#ff5252", "#448aff"],
+                            data: [response[0], response[1]]
+                        }
+                    ]
+                },
+                options: {
+                    legend: { display: false },
+                    title: {
+                        display: true,
+                        text: 'Predicted Chronic Kidney Disease'
+                    },
+                    scales: {
+                        yAxes: [
+                            {
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }
+                        ]
+                    }
+                }
+            });
+        },
+        error: () => {
+
+        },
+        method: "POST",
+        dataType: "json"
+    })
+}
+
+function resetTable() {
+    $("#train-data-div *").remove()
+    $("#test-data-div *").remove()
+    $("#train-data-div").append(`
+    <table class="table table-striped table-bordered dt-responsive nowrap" id="train-table" style="width:100%">
+        <thead><tr></tr></thead>
+        <tbody></tbody>
+    </table>`)
+    $("#test-data-div").append(`
+    <table class="table table-striped table-bordered dt-responsive nowrap" id="test-table" style="width:100%">
+        <thead><tr></tr></thead>
+        <tbody></tbody>
+    </table>`)
+}
+
+function showFormData(formData) {
+    for (var pair of formData.entries()) {
+        console.log(pair[0], pair[1])
+    }
+}
+
+function showResponse(id, response, key) {
+    var table = $(id)
+    const response_data = response[key]
+    const header = Object.keys(response_data[0])
+    header.forEach((v) => {
+        table.find("thead").find("tr").append(`<th>${v}</th>`)
+    })
+    response_data.forEach((v) => {
+        var line = `<tr>`
+        for (val in v) {
+            line += `<td>${v[val]}</td>`
+        }
+        line += `</tr>`
+        table.find("tbody").append(line)
+    })
+
+    if (!$.fn.DataTable.isDataTable(table)) {
+        table.DataTable({
+            pageLength: 5,
+            lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'All']],
+            responsive: true
+        })
+    }
+}
---
+++ controller/controller.php
@@ -0,0 +1,201 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    // print_r($_POST);
+    // STEP 1
+    if ($_POST["step"] === "1") {
+        // get file data
+        $train_data_file = $_FILES["train_data"];
+        $test_data_file = $_FILES["test_data"];
+
+        $response = [];
+        $response["train"] = readCSV($train_data_file);
+        $response["test"] = readCSV($test_data_file);
+
+        echo json_encode($response);
+    }
+
+    // STEP 2
+    else if ($_POST["step"] === "2") {
+        $data = $_POST["request_data"];
+        $attr = $_POST["attr"];
+
+        foreach ($attr as $index => $eachattribute) {
+            if($eachattribute["checked"] == "false") {
+                foreach ($data["train"] as $i => $x) {
+                    unset($data["train"][$i][$eachattribute["val"]]);
+                }
+                foreach ($data["test"] as $i => $x) {
+                    unset($data["test"][$i][$eachattribute["val"]]);
+                }
+            }
+        }
+        echo json_encode($data);
+    }
+
+    // STEP 3
+    else if ($_POST["step"] === "3") {
+        $data = $_POST["request_data"];
+        $label = $_POST["label"];
+
+        foreach ($data["train"] as $i => $x) {
+            unset($data["train"][$i][$label]);
+        }
+        foreach ($data["test"] as $i => $x) {
+            unset($data["test"][$i][$label]);
+        }
+
+        echo json_encode($data);
+    }
+
+    // STEP 4
+    else if ($_POST["step"] === "4") {
+        // get unique data
+        $data = $_POST["request_data"];
+        $header = array_keys($data["train"][0]);
+        $unique = [];
+        foreach ($header as $index => $k) {
+            // setiap headernya dicari yang unique
+            if(!is_numeric($data["train"][0][$k])) {
+                $unique[$k] = getUnique($k, $data["train"]);
+            }
+        }
+        
+        // parse number
+        foreach ($header as $index => $k) {
+            if(!is_numeric($data["train"][0][$k])) {  
+                // parse
+                foreach ($data["train"] as $i => $x) {
+                    $data["train"][$i][$k] = array_search($data["train"][$i][$k] , $unique[$k]);
+                }
+            }
+
+            if(!is_numeric($data["test"][0][$k])) { 
+                // parse 
+                foreach ($data["test"] as $i => $x) {
+                    $data["test"][$i][$k] = array_search($data["test"][$i][$k] , $unique[$k]);
+                }
+            }
+        }
+
+        echo json_encode($data);
+    }
+
+    // STEP 5
+    else if ($_POST["step"] === "5") {
+        
+        $data = $_POST["request_data"];
+        $raw_data = $_POST["final_data"];
+        $label = $_POST["label"];
+        $header = array_keys($data["train"][0]);
+        // distances isinya 
+        // train => index train nya
+        // test => index test nya
+        // distance => distance nya
+        for($x = 0; $x < count($data["test"]); $x++) {
+            $distances = [];
+            for($y = 0; $y < count($data["train"]); $y++) {
+                $calculated_dis = calculateDistance($data["train"], $data["test"], $y, $x, $header);
+                $distance = [
+                    "test" => $x,
+                    "train" => $y,
+                    "distance" => $calculated_dis
+                ];
+
+                array_push($distances, $distance);
+            }
+            // sort
+            usort($distances, function($a, $b){
+                return $a["distance"] <=> $b["distance"];
+            });
+
+            // take 3 min distance
+            // TODO: bikin k nya dilempar dari html
+            $k = 3;
+            $labels = [];
+            $test_index = $x;
+            for ($i=0; $i < $k; $i++) { 
+                $train_index = $distances[$i]["train"];
+                array_push($labels, $raw_data["train"][$train_index][$label]);
+            }
+            
+            $arr_freq = array_count_values($labels);
+            arsort($arr_freq);
+            $new_arr = array_keys($arr_freq);
+            $raw_data["test"][$x][$label] = $new_arr[0];
+            // var_dump($label, $new_arr[0]);
+            // print_r($raw_data["test"][$x]);
+        }
+
+        echo json_encode($raw_data);
+    }
+
+    // CHART
+    else if ($_POST["step"] === "chart") {
+        $data = $_POST["final_data"];
+
+        $ckd_count = 0;
+        $nckd_count = 0;
+        foreach ($data["test"] as $key => $value) {
+            if ($value["classification"] == "ckd")
+                $ckd_count++;
+            else
+                $nckd_count++;
+        }
+
+        $response = [];
+        $response[0] = $ckd_count;
+        $response[1] = $nckd_count;
+
+        echo json_encode($response);
+    }
+}
+
+function calculateDistance($train, $test, $indexoftrain, $indexoftest, $header) {
+    $temp = 0;
+    foreach ($header as $h) {
+        $temp += (pow($test[$indexoftest][$h]-$train[$indexoftrain][$h] , 2));
+    }
+    return $temp;
+}
+
+function getUnique($header, $data) {
+    $unique = [];
+    foreach ($data as $i => $d) {
+        array_push($unique, $d[$header]);
+    }
+    $unique = array_unique($unique);
+    $reset_unique = array_values($unique);
+    sort($reset_unique);
+    return $reset_unique;
+}
+
+function readCSV($data_file) {
+    $header = [];
+    $array = [];
+    if($file = fopen($data_file["tmp_name"], "r")) {
+        $header = fgetcsv($file);
+
+        $index = 0;
+        while($temp_train = fgets($file)) {
+            // per line
+            $temp_train = explode(',',$temp_train);
+            foreach ($temp_train as $key => $value) {
+                // per column
+                $value = trim($value);
+                if($value === null || $value === "" || strlen($value) === 0 || $value === "?") {
+                    array_splice($array, $index, 1);
+                    $index--;
+                    break;
+                }
+
+                $array[$index][$header[$key]] = $value;
+            }
+            $index++;
+        }
+        // print_r(explode(",",$array[31]["id"]));
+        // print_r($array[31]);
+        return $array;
+    }
+    return false;
+}
